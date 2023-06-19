ccff7cba5c63dbd646c26880065b43059dad19d2
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-06-19 23:36:57 +0200

    bind parent

--- src/actions/workflow/index.ts
+++ src/actions/workflow/index.ts
@@ -4,17 +4,31 @@ import { TineActionOptions } from '../../types';
 import rpc from '../rpc';
 import shape from '../shape';
 
+const isNested = (path: string) => {
+  const dotRegex = /\./g;
+  const matches = path.match(dotRegex);
+  return matches && matches.length > 1;
+};
+
+const getParent = (path: string) => path.split('.').slice(0, -1).join('.');
+
 const workflow = tineAction(
   async (payload: object, { ctx }: TineActionOptions) => {
     let res = null;
 
     for (const [name, actionDef] of Object.entries(payload)) {
-      const action =
+      let action =
         BASE_ACTIONS[actionDef.action] ||
         get(ctx.get('.tine-workflow-actions'), actionDef.action);
 
       if (!action) {
         throw new Error('Action not found');
+      }
+
+      if (!BASE_ACTIONS[actionDef.action] && isNested(actionDef.action)) {
+        action = action.bind(
+          get(ctx.get('.tine-workflow-actions'), getParent(actionDef.action)),
+        );
       }
 
       res = await action(actionDef.payload, { name }).run(ctx);
