fde592f595e20d294b5ea910102c26568904d25a
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-08-31 13:05:03 +0200

    add parallel

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,5 +3,6 @@ export { default as shape } from './shape';
 export { default as task } from './task';
 export { default as workflow } from './workflow';
 export { default as process } from './process';
+export { default as parallel } from './parallel';
 export { default as response } from './response';
 export { default as rpc } from './rpc';
