cf76b24d0a84f171637e546492f20788b8638e2b
Bersen Pajaziti <pajaziti.bersen@gmail.com>
2023-12-08 13:43:43 +0100

    test

--- src/tineHelpers.ts
+++ src/tineHelpers.ts
@@ -5,8 +5,4 @@ import { DEFAULT_ACTIONS } from './defaultActions';
 export const tineCtx = (value?: object): Map<any, any> =>
   new Map(Object.entries({ ...value, '.defaultActions': DEFAULT_ACTIONS }));
 
-export const tineInput = <T>(
-  schema: z.ZodType<T>,
-  args?: { name?: string },
-): z.ZodType<T> =>
-  Object.assign(schema, { ...args, name: args?.name || 'input' });
+export const tineInput = z.object;
