3a51e25c8561357bdbdc165ea62aacdd25b18d47
Bersen Pajaziti <bersenp@hotmail.com>
2024-01-06 10:59:27 +0100

    add types

--- src/zod.ts
+++ src/zod.ts
@@ -4,3 +4,21 @@ import { z } from 'zod';
 extendZodWithOpenApi(z);
 
 export { z };
+
+const PRIMITIVES = ['number', 'boolean'];
+
+export function getType(rawType: string) {
+  if (PRIMITIVES.includes(rawType)) {
+    return `z.coerce.${rawType}()`;
+  }
+
+  return `z.string()`;
+}
+
+export function getEncodedType(type: string) {
+  if (type.includes('coerce')) {
+    return type.split('z.coerce.')[1].replace(/\(\)/g, '');
+  }
+
+  return type.split('z.')[1].replace(/\(\)/g, '');
+}
