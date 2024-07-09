3acc716b3d2faf16d5fd528ecaa6edabe861596f
Bersen Pajaziti <bersenp@hotmail.com>
2024-07-09 13:55:47 +0200

    extend struct

--- src/actions/struct/index.ts
+++ src/actions/struct/index.ts
@@ -1,12 +1,19 @@
 import { $action } from "../../daunus_action";
+import { DaunusCtx } from "../../types";
+
+function isFunction<T>(
+  param: T | ((ctx: DaunusCtx) => T)
+): param is (ctx: DaunusCtx) => T {
+  return typeof param === "function";
+}
 
 const struct = $action(
   {
     type: "struct"
   },
-  () =>
-    <T>(params: T) =>
-      params!
+  ({ ctx }) =>
+    <T>(params: T | ((ctx: DaunusCtx) => T)) =>
+      isFunction(params) ? params(ctx) : params!
 );
 
 export default struct;
