511f4b944579dcd9072f6226a5132f3d7140ab9d
Bersen Pajaziti <bersenp@hotmail.com>
2024-07-09 14:12:47 +0200

    fix

--- src/example/login.ts
+++ src/example/login.ts
@@ -1,27 +1,16 @@
-import {
-  $input,
-  condition,
-  $var,
-  exit,
-  z,
-  struct,
-  $fn,
-  DaunusException
-} from "..";
+import { $input, condition, $var, exit, z, struct, DaunusException } from "..";
 
 const input = $input({
   did: z.string().describe("Decentralized Identifier token")
 }).openapi("Login Input");
 
-const magicMeta = struct(
-  $fn(() => {
-    if (Math.random() > 0.5) {
-      return { email: "test@example.com" };
-    }
+const magicMeta = struct(() => {
+  if (Math.random() > 0.5) {
+    return { email: "test@example.com" };
+  }
 
-    return new DaunusException(403, "Forbidden");
-  })
-);
+  return new DaunusException(403, "Forbidden");
+});
 
 const user = struct({
   where: { email: $var(magicMeta, "email") }
