86a1b052384bb77ba0518da547ad34d2e7fc3e7a
Bersen Pajaziti <bersenp@hotmail.com>
2024-07-13 00:15:29 +0200

    httpInput

--- src/daunus_helpers.ts
+++ src/daunus_helpers.ts
@@ -1,3 +1,4 @@
+import { ZodRawShape } from "zod";
 import { z } from "./zod";
 
 import { DEFAULT_ACTIONS } from "./default_actions";
@@ -6,3 +7,6 @@ export const $ctx = (value?: object): Map<any, any> =>
   new Map(Object.entries({ ...value, ".defaultActions": DEFAULT_ACTIONS }));
 
 export const $input = z.object;
+
+export const $httpInput = <T extends ZodRawShape>(shape: T) =>
+  z.object({ _type: z.literal("http"), ...shape });
