9dd024ea097a24c67b50314d0158d8cc3c0847aa
Bersen Pajaziti <bersenp@hotmail.com>
2024-10-20 23:54:14 +0200

    convert to function

--- src/daunus_steps.ts
+++ src/daunus_steps.ts
@@ -1,4 +1,4 @@
-import { exit, struct } from "./actions";
+import { struct } from "./actions";
 import {
   ResolveDaunusVarData,
   DaunusGetExceptions,
@@ -11,78 +11,44 @@ type StepsContext<T extends Record<any, any>> = {
     : T[TKey];
 };
 
-export class $steps<S extends Record<string, any> = {}> {
-  public __scope: S;
+function $steps<S extends Record<string, any> = {}>(initialScope?: S) {
+  const scope: S = initialScope || ({} as S);
 
-  constructor(scope?: S) {
-    this.__scope = scope = {} as S;
-  }
+  function add<T, N extends string>(name: N, fn: ($: StepsContext<S>) => T) {
+    const result = fn(scope);
 
-  add<T, N extends string>(name: N, fn: ($: StepsContext<S>) => T) {
-    return new $steps<S & Record<N, T>>({
-      ...(this.__scope || ({} as S)),
-      [name]: fn(this.__scope || ({} as S))
+    return $steps<S & Record<N, T>>({
+      ...scope,
+      [name]: result
     });
   }
 
-  get<N extends keyof S>(name: N): S[N] {
-    return this.__scope[name];
+  function get<N extends keyof S>(name: N): S[N] {
+    return scope[name];
   }
+
+  return {
+    scope,
+    add,
+    get
+  };
 }
 
-export class $loop<S extends Record<string, any> = {}> {
-  public __scope: S;
-  private params: { list: Array<any>; itemName: string };
-
-  constructor(
-    { itemName = "item", list }: { list: Array<any>; itemName?: string },
-    scope?: S
-  ) {
-    this.__scope = scope ?? ({} as S);
-    this.params = { itemName, list };
-  }
-
-  add<T, N extends string>(name: N, fn: (defs: StepsContext<S>) => T) {
-    return new $loop<S & Record<N, T>>(this.params, {
-      ...(this.__scope || ({} as S)),
-      [name]: fn(this.__scope || ({} as S))
-    });
-  }
-
-  get<N extends keyof S>(name: N): S[N] {
-    return this.__scope[name];
-  }
+function $loop<
+  L extends Array<any>,
+  I extends string = "item",
+  S extends Record<string, any> = {}
+>({ itemName = "item" as I }: { list: L; itemName?: I }, initialScope?: S) {
+  return $steps(initialScope).add(itemName!, () => {
+    return { value: {} as any as keyof L, index: {} as number };
+  });
 }
 
-export class $condition<S extends Record<string, any> = {}> {
-  public __scope: S;
-  private params: { list: Array<any>; itemName: string };
-
-  constructor(
-    { itemName = "item", list }: { list: Array<any>; itemName?: string },
-    scope?: S
-  ) {
-    this.__scope = scope ?? ({} as S);
-    this.params = { itemName, list };
-  }
-
-  add<T, N extends string>(name: N, fn: (defs: S) => T) {
-    return new $loop<S & Record<N, T>>(this.params, {
-      ...(this.__scope || ({} as S)),
-      [name]: fn(this.__scope || ({} as S))
-    });
-  }
-
-  get<N extends keyof S>(name: N): S[N] {
-    return this.__scope[name];
-  }
-}
-
-const lorem = new $steps()
-  .add("lorem", () => struct([1, 2, 3]))
+const lorem = $steps()
+  .add("lorem", () => struct([{ asd: true }, 2, 3]))
   .add("loop", ($) =>
-    new $loop({ list: $.lorem.data, itemName: "item" }, $)
-      .add("lorem2", ($) => exit({ status: 500, data: { name: "asdasd" } }))
-      .add("ipsum", ($) => struct($.lorem2.exception.data))
+    $loop({ list: $.lorem.data }, $)
+      .add("ipsum", ($) => struct($.lorem.data))
+      .add("trip", ($) => struct($.item.value))
   )
-  .add("trip", () => struct({ name: "test" }));
+  .add("trip2", () => struct({ name: "test" }));
