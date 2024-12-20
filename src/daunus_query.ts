626deaceb94e47cd3c16b6fc81982684274456aa
Bersen Pajaziti <bersenp@hotmail.com>
2024-12-20 22:39:16 +0100

    format

--- src/daunus_query.ts
+++ src/daunus_query.ts
@@ -11,14 +11,18 @@ function createNestedProxy<T extends NestedMap>(target: T): any {
         target.paths
       ) {
         const value = target.paths[prop];
+
         if (
           value instanceof Map ||
           (typeof value === "object" && value !== null)
         ) {
           return createNestedProxy(value);
         }
+
         return value;
-      } else if (typeof prop === "string" && target instanceof Map) {
+      }
+
+      if (typeof prop === "string" && target instanceof Map) {
         const value = target.get(prop);
         if (
           value instanceof Map ||
@@ -26,38 +30,49 @@ function createNestedProxy<T extends NestedMap>(target: T): any {
         ) {
           return createNestedProxy(value);
         }
+
         return value;
-      } else if (typeof prop === "string" && prop in target) {
+      }
+
+      if (typeof prop === "string" && prop in target) {
         const value = Reflect.get(target, prop, receiver);
+
         if (
           value instanceof Map ||
           (typeof value === "object" && value !== null)
         ) {
           return createNestedProxy(value);
         }
+
         return value;
       }
 
       return Reflect.get(target, prop, receiver);
     },
+
     set(target, prop, value, receiver) {
       if (typeof prop === "string" && target instanceof Map) {
         target.set(prop, value);
+        
         return true;
-      } else {
-        return Reflect.set(target, prop, value, receiver);
       }
+
+      return Reflect.set(target, prop, value, receiver);
     },
+
     has(target, prop) {
       if (typeof prop === "string" && target instanceof Map) {
         return target.has(prop);
       }
+
       return Reflect.has(target, prop);
     },
+
     deleteProperty(target, prop) {
       if (typeof prop === "string" && target instanceof Map) {
         return target.delete(prop);
       }
+
       return Reflect.deleteProperty(target, prop);
     }
   });
@@ -68,6 +83,7 @@ export function $query<R>(
 ): DaunusQuery<R> {
   const $query = async (ctx: DaunusCtx) => {
     const $ = createNestedProxy(ctx);
+    
     return await selector($);
   };
 
