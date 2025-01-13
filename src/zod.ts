d1943e6d950ba5ef613cb5292bab0da062e04d29
Bersen Pajaziti <bersenp@hotmail.com>
2025-01-13 22:59:05 +0100

    clean

--- src/zod.ts
+++ src/zod.ts
@@ -32,21 +32,3 @@ declare module "zod" {
 
 // eslint-disable-next-line unicorn/prefer-export-from
 export { z }
-
-const PRIMITIVES = new Set(["number", "boolean"])
-
-export function getType(rawType: string) {
-  if (PRIMITIVES.has(rawType)) {
-    return `z.coerce.${rawType}()`
-  }
-
-  return `z.string()`
-}
-
-export function getEncodedType(type: string) {
-  if (type.includes("coerce")) {
-    return type.split("z.coerce.")[1].replace(/\(\)/g, "")
-  }
-
-  return type.split("z.")[1].replace(/\(\)/g, "")
-}
