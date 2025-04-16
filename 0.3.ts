3b608bd61a620681f5bf0a09a74003ad95631bd2
Bersen Pajaziti <bersenp@hotmail.com>
2025-04-16 22:56:37 +0200

    0.3 concept

--- 0.3.ts
+++ 0.3.ts
@@ -1,25 +1,50 @@
 type Trumpet<Name, Scope> = [Name, (props: { scope: Scope }) => any]
 
-function Steps<const T0, const T1, const T2, const T3>(
-  ...trumpets: [
-    Trumpet<T0, {}>,
-    Trumpet<T1, T0>,
-    Trumpet<T2, T1>,
-    Trumpet<T3, T2>
-  ]
-) {}
+interface Steps {
+  <const T0>(...trumpets: [Trumpet<T0, {}>]): number
+  <const T0, const T1>(...trumpets: [Trumpet<T0, {}>, Trumpet<T1, T0>]): number
+  <const T0, const T1, const T2>(
+    ...trumpets: [Trumpet<T0, {}>, Trumpet<T1, T0>, Trumpet<T2, T1>]
+  ): number
+  <const T0, const T1, const T2, const T3>(
+    ...trumpets: [
+      Trumpet<T0, {}>,
+      Trumpet<T1, T0>,
+      Trumpet<T2, T1>,
+      Trumpet<T3, T2>
+    ]
+  ): number
+  <const T0, const T1, const T2, const T3, const T4>(
+    ...trumpets: [
+      Trumpet<T0, {}>,
+      Trumpet<T1, T0>,
+      Trumpet<T2, T1>,
+      Trumpet<T3, T2>,
+      Trumpet<T4, T3>
+    ]
+  ): number
+}
 
-const cw = Steps(
-  ["greet", () => `Hello`],
+const Steps = (() => {
+  return {} as any
+}) as Steps
 
-  [
-    "double",
-    () => ({
-      asd: "asdad"
-    })
-  ],
+const UseCase = (name: string) => {
+  const steps = (() => {
+    return name as any
+  }) as Steps
 
-  ["greet2", ({ scope }) => scope],
+  return {
+    steps,
+    input: () => ({ steps })
+  }
+}
 
-  ["greet4", ({ scope }) => scope]
-)
+const hello = UseCase("Hello")
+  .input()
+
+  .steps(
+    ["first step", () => Math.random() > 0.5],
+
+    ["second step", ({ scope }) => scope]
+  )
